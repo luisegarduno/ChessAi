3ebcb0ff3a90be56aa0bed021b4a6d0f194f67c0
luisegarduno <luis.e.garduno@gmail.com>
2022-10-14 00:53:52 -0500

    - Created initial main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * EnigmaChess v0.0.1
+ * ------------------
+ * Created by Luis G.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[]){
+
+  cout << "Enigma Chess" << endl;
+
+  return 0;
+}
